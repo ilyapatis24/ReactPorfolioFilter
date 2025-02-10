97a304022d03770a4e200538d4fd47587f5d3972
ilyapatis24 <ilyapatis24@mail.ru>
2025-02-11 01:53:52 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import React from "react";
+import Portfolio from "./components/portfolio";
+
+const App = () => (
+  <div>
+    <Portfolio />
+  </div>
+);
+
+export default App;
